c9e9d8919592422bd5ac8f63ef70fcb9578ad8f6
ryo.terayama <alice061o@outlook.com>
2020-09-12 14:14:08 +0900

    Add Follow

---
+++ src/controllers/follows/FollowCreateServlet.java
@@ -0,0 +1,53 @@
+package controllers.follows;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
+import models.Follow;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class FollowCreateServlet
+ */
+@WebServlet("/follow/create")
+public class FollowCreateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FollowCreateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = (String)request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            Follow f = new Follow();
+            Employee a = em.find(Employee.class, Integer.parseInt(request.getParameter("emp_id")));
+
+
+            f.setFollowed_id(a);
+            f.setFollower_id((Employee)request.getSession().getAttribute("login_employee"));
+
+            em.getTransaction().begin();
+            em.persist(f);
+            em.getTransaction().commit();
+            em.close();
+        }
+    }
+
+}
--- src/models/Follow.java
+++ src/models/Follow.java
@@ -2,29 +2,62 @@ package models;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Table(name = "follows")
+
+@NamedQueries({
+
+    @NamedQuery(//フォローしているかを確認
+            name = "followedCheck",
+            query = "select count(f) from Follow AS f WHERE f.follower_id = :logid AND f.followed_id = :employee "),
+    @NamedQuery(//フォローしている時フォロー情報を取得
+            name = "getFollowData",
+            query = "select f from Follow AS f WHERE f.follower_id = :logid AND f.followed_id = :employee "),
+    @NamedQuery(//フォロワー数を取得
+            name = "getFollowerCount",
+            query = "select count(f) from Follow AS f WHERE f.followed_id = :employee "),
+})
 
 @Entity
 public class Follow {
+
     @Id
-    @Column(name = "emp_id" ,  nullable = false)
-    private Integer emp_id;
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-    @Column(name = "follow_id" ,  nullable = false)
-    private Integer follow_id;
+    @Column(name = "followed_id" ,  nullable = false)
+    private Integer followed_id;
+
+    @Column(name = "follower_id" ,  nullable = false)
+    private Integer follower_id;
 
 
-    public Integer getEmpId() {
-        return emp_id;
+    public Integer getId() {
+        return id;
     }
 
-    public void setEmp_id(Integer emp_id) {
-        this.emp_id = emp_id;
+    public void setId(Integer id) {
+        this.id = id;
     }
-    public Integer getFollowId() {
-        return follow_id;
+
+    public Integer getFollowed_id() {
+        return followed_id;
     }
-    public void setFollow_id(Integer follow_id) {
-        this.follow_id = follow_id;
+
+    public void setFollowed_id(Integer followed_id) {
+        this.followed_id = followed_id;
+    }
+    public Integer getFollower_id() {
+        return follower_id;
+    }
+    public void setFollower_id(Integer follower_id) {
+        this.follower_id = follower_id;
     }
 }
